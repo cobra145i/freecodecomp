1a64b2a688ec82d7d3e8f8534400ba200efd15d9
Igor Trushin <cobra145@mail.ru>
2025-04-21 18:49:31 +0300

    8kyu - Task Method (Math.floor)

---
+++ codewars/8 kyu/KeepHydrated!/code.js
@@ -0,0 +1,10 @@
+function litres(time) {
+    let res = Math.floor(time * 0.5)
+     console.log(res)
+    }
+
+    litres(0)//0, 'litres(0) should return 0');
+    litres(1)//0, 'litres(1) should return 0');
+    litres(2)//, 1, 'litres(2) should return 1');
+    litres(3)//, 1, 'litres(3) should return 1');
+    litres(4)//, 2, 'litres(4) should return 2');
