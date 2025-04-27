d1c56ae76d185fc592c7c121afa85638f8ce4768
Igor Trushin <cobra145@mail.ru>
2025-04-27 23:21:29 +0300

    8 kyu Traning 5

--- codewars/8 kyu/RemovingElements/index.js
+++ codewars/8 kyu/RemovingElements/index.js
@@ -1,6 +1,9 @@
 function removeEveryOther(arr){
-    //your code here
-  }
+   
+    const res = arr.filter(index => index % 2 === 0);
+    
+    console.log(res); // [2, 4, 6]
+}
   removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])//,['Hello', 'Hello Again']);
   removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])//,[1, 3, 5, 7, 9]);
   removeEveryOther([[1, 2]])//, [[1, 2]]);
