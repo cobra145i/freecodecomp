0c89f151abc502edef1c694bdd9c693e808c58d5
Igor Trushin <cobra145@mail.ru>
2025-04-27 21:03:31 +0300

    Task 8 kyu Removing Elements

---
+++ codewars/8 kyu/RemovingElements/index.js
@@ -0,0 +1,7 @@
+function removeEveryOther(arr){
+    //your code here
+  }
+  removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])//,['Hello', 'Hello Again']);
+  removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])//,[1, 3, 5, 7, 9]);
+  removeEveryOther([[1, 2]])//, [[1, 2]]);
+  removeEveryOther([['Goodbye'], {'Great': 'Job'}])//,[['Goodbye']]);
