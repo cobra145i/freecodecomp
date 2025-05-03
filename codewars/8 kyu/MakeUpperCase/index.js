a6ab0e6ba6f9ef00b74a891d44180a58f945af94
Igor Trushin <cobra145@mail.ru>
2025-05-04 00:47:47 +0300

    Task 8 kyu Make Upper Case

---
+++ codewars/8 kyu/MakeUpperCase/index.js
@@ -0,0 +1,6 @@
+const makeUpperCase = str => str.toUpperCase()
+  
+  makeUpperCase("")//,           "");
+  makeUpperCase("hello")//, "HELLO");
+  makeUpperCase("Hello")//, "HELLO");
+  makeUpperCase("HELLO")//, "HELLO");
