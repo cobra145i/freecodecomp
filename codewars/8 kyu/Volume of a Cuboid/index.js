ce44964e4fa5a18465a3607befb133c57d3fb55c
Igor Trushin <cobra145@mail.ru>
2025-05-04 15:52:06 +0300

    Task 8 kyu  Volume of a Cuboid

---
+++ codewars/8 kyu/Volume of a Cuboid/index.js
@@ -0,0 +1,9 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      let res = length * width * height
+      return res
+    }
+  }
+
+  getVolumeOfCuboid(1,2,2)//,  4);
+  getVolumeOfCuboid(6,2,5)//, 60);
